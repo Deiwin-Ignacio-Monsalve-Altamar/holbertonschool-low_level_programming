e1f3fd66f0632b61b3bade14a516443cd9656a6f
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-05 14:32:01 -0500

    Tasks 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main() 
+{ 
+    printf("Size of a char:%lu\n", sizeof(char)); 
+    printf("Size of an int:%lu\n", sizeof(int));
+    printf("Size of a long int%lu\n", sizeof(long int));
+    printf("Size of a long long int int:%ld\n", sizeof(long long int));
+    printf("Size of a float:%lu\n", sizeof(float)); 
+    return 0; 
+} 
