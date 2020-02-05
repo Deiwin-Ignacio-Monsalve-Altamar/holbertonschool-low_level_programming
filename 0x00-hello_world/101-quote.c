7275503ec94874b586e3039f63236faf39368841
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-05 17:46:52 -0500

    Tasks advance 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
 * main - main function
@@ -6,6 +5,6 @@
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 return (1);
 }
