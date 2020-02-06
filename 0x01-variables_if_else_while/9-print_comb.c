2709f3af148488b70b0aac56641577cc1a033c86
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-06 16:53:34 -0500

    Task 9 conditionals

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+char letter;
+
+for (n = '0' ; n <= '9' ; n++)
+{
+purchar(n);
+if (n < 9)
+{
+purchar(' ');
+purchar(',');
+}
+else
+{
+purchar('$');
+}
+}
+putchar('\n');
+return (0);
+}
