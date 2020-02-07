d042fb72769cecc6663f67a9d310ea795aac5233
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-07 08:06:47 -0500

    Task 11 conditionals

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x, z;
+for (x = '0' ; x <= '8' ; x++)
+{
+for (z = x + '0' ; z <= '9' ; z += 2)
+{
+putchar(x);
+putchar(z);
+
+if (!(x == '8' && z == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
