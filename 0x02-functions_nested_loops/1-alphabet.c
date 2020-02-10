2d0b63853ead55b76cb59bd040c513e9d1f20fae
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-10 15:14:49 -0500

    tasks 2 prints

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
