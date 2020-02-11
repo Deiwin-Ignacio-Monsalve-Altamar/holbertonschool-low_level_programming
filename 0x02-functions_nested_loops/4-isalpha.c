a5c185f0e42022b0d3f3f2352c98d6c4f1069efc
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-11 12:48:59 -0500

    Tasks

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isalpha - prints checkks for or uppercase.
+ **@c: the integer to print
+ * Return:0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
