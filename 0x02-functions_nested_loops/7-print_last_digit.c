13a90a18dcde35d3cbd68a00f38917ab582f1d64
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-11 17:34:26 -0500

    library

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 /**
  *print_last_digit - print last digit
- *@n: the integer to print
+ *@n: integer print
  * Return: always 0
  */
 int print_last_digit(int n)
 {
-	int x;
 
 	if (n < 0)
 	{
 		n = n * -1;
 	}
-	x = n % 10;
-	_putchar(x + '0');
-	return (x);
+	n = n % 10;
+	_putchar(n + '0');
+	return (n);
 }
