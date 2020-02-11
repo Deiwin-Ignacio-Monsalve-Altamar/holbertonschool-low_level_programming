938d37aacec18900be28e25e342fda5576e9f316
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-11 15:41:43 -0500

    Tasks 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
 /**
- * times_table - print time reloj
+ * times_table - print time
  *
  * Return: 0
  */
 void times_table(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i, j, r, res;
+
+	for (i = 0; i <= 9; i++)
 	{
-		int r = i * 0;
+		r = i * 0;
 
 		_putchar('0' + r);
-		for (int j = 1; j <= 9; j++)
+		for (j = 1; j <= 9; j++)
 		{
-			int res = i * j;
+			res = i * j;
 
 			if (res > 9)
 			{
