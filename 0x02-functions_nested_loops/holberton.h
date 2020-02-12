63073868c6ddd73afdbab9a879bcf4600beaf9d0
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-11 21:52:07 -0500

    tasks

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,6 +7,7 @@ void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int n);
 void jack_bauer(void);
 void times_table(void);
 int print_last_digit(int n);
