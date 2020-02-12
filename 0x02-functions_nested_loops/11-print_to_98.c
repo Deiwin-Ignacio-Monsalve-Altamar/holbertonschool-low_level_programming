773840f8f6e2ae217ed8a7cb32af73583346080a
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-12 12:50:45 -0500

    tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,54 +2,30 @@
 #include <stdio.h>
 /**
  * print_to_98 - print
- *
+ * @n: integer
  * Return: 0
  */
 void print_to_98(int n)
 {
-	if (n != 98){
-	if (n == 0)
-  	{
-		for (int i = '0'; i <= 98; i++)
-		{
-					_putchar((char)i + '0');
-					_putchar(',');
-					_putchar(' ');
-		}
-	}
-	else if (n == 98)
+	if (n < 98)
 	{
-		_putchar((char)n + '0');
-	}
-	else if (n > 98)
-	{
-		for (int i = 111; 11 >= 98; i--)
+		for (; n <= 98; n++)
 		{
-				_putchar((char) i + '0');
-				_putchar(',');
-				_putchar(' ');
-		}
-	}
-	else if (n < 98)
-	{
-		for (int i = 81; i <= 98; i++)
-		{
-			
-			_putchar((char) i + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d\n", n);
 		}
 	}
 	else
 	{
-		for (int i = -10; i <= 98; i++)
+		for (; n >= 98; n--)
 		{
-				_putchar((char) i + '0');
-				_putchar(',');
-				_putchar(' ');
-       
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d\n", n);
 		}
 	}
-	}
-	_putchar('\n');
 }
+
