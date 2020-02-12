756066fcee5a518443e63bbfc6eafff91a9c0378
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-11 21:19:03 -0500

    tasks

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - (don’t delete it!).
+* main - out (don’t delete it!) the part of the code that is causing the output to go into an infinite loop.
 * Return: 0
 */
 
