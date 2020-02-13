8ee40c74e81ce1d4724ad4b645345c1742d6c4db
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-13 11:00:38 -0500

    Tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - prints operation maths
+ * @a: integer
+ * @b: integer
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,9 @@
 #ifndef HOLBERTON
 #define HOLBERTON
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 #endif
