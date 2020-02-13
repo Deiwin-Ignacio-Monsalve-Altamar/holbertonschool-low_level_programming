cb03ddb1adb6641bbaaef9d7def956c31c344238
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-12 22:01:25 -0500

    tasks

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+	unsigned long int a = 1, b = 1, i, c, sum = 0;
+
+	for (i = 1; i <= 4000000; i++)
+	{
+	c = a + b;
+	if ((c % 2) == 0)
+		sum += c;
+
+	a = b;
+	b = c;
+	}
+	printf("%lu/n", sum);
+	return (0);
+}
