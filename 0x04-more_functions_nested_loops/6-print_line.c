07dc55c55a1c149662273788551dc19b5ab0a36a
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-13 13:13:20 -0500

    Tasks 1

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,26 +1,28 @@
 #include "holberton.h"
-
 /**
  * print_line - print numbers
- *Return: Always 0
+ *
+ * @n: integer
+ *
+ * Return: Always 0
  */
 
-void void print_line(int n);
+void print_line(int n)
 {
 	int i;
 
-	if (n != 0)
+	if (n > 0)
 	{
 		i = 1;
-		while ( i <= n)
+		while (i <= n)
 		{
 		_putchar('_');
 		i++;
+		}
+		_putchar('\n');
 	}
 	else
 	{
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
-
