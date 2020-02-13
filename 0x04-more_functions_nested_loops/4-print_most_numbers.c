2541737c6a8fd946ee2a2e9e29d71a3783307824
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-13 11:10:13 -0500

    Tasks

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print numbers
+ *Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
