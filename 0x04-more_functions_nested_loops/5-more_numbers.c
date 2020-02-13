72bc3b1455ce82464d5e992a6134febbe59363ec
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-13 11:26:22 -0500

    Tasks

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print ten times the numbers;
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j <= 9)
+			{
+				_putchar('0' + j);
+			}
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
