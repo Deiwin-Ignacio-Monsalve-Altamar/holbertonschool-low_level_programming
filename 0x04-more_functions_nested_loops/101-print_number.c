7130a830bf57a180f4b96db9b35b4d347069a34e
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-13 23:07:33 -0500

    Advanced

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_number - print number integer
+ *
+ * @n: Integer
+ * Return: Always 0
+ */
+void print_number(int n)
+{
+	if (n > 99)
+	{
+		_putchar((char)((n / 100) / 10) + '0');
+		_putchar((char)(n / 10) + '0');
+	}
+	else if (n > 999)
+	{
+		_putchar((char)(((n / 1000) / 100) / 10) + '0');
+		_putchar((char)((n / 100) / 10) + '0');
+		_putchar((char)(n / 10) + '0');
+	}
+	else
+	{
+		_putchar((char)(n / 10) + '0')
+	}
+	_putchar((char)(n % 10) + '0');
+}
+
