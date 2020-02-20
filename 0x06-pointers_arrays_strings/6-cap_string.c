e0a00286bd2bbbc6fafb3f2e22b862031f9d36f7
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-20 17:18:22 -0500

    tasks 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,28 +11,28 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 48 || str[i] == '	' || str[i] == '\n')
+		if (str[i] == ' ' || str[i] == '	' || str[i] == '\n')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
 		}
-		if (str[i] == 33 || str[i] == 63 || str[i] == 34 || str[i] == 40)
+		else if (str[i] == 33 || str[i] == 63 || str[i] == 34 || str[i] == 40)
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
 		}
-		if (str[i] == 41 || str[i] == '{' || str[i] == '}' || str[i] == 46)
+		else if (str[i] == 41 || str[i] == '{' || str[i] == '}' || str[i] == 46)
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
 		}
-		if (str[i] == 44 || str[i] == 59)
+		else if (str[i] == 44 || str[i] == 59)
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
