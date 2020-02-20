35574442a151afbc366ff926bc99cc3c9b842d99
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-20 02:51:13 -0500

    tasks 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	while(*dest)
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		dest++;
+		i++;
 	}
 
-	while(*src)
+	j = 0;
+	while ((dest[i++] = src[j++]) != '\0')
 	{
-		*dest = *src;
-		src++;
-		dest++;
 	}
-	*dest = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
 
