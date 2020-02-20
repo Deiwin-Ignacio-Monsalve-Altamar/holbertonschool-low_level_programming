4b1e9c05cd14d0f6698c215dec0036d4277a91d2
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-20 14:20:00 -0500

    tasks 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * *_strcmp - print character
+ * @s1: pointr character
+ * @s2 : poinr character
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+		if (s1[i] == '\0')
+		return (0);
+	return (*s1 - *s2);
+}
+
