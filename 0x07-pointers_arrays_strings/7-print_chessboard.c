0a3ace15968d2a4b2dba63c8226d9e3b75b0b296
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-24 16:44:17 -0500

    tasks 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_chessboard - print character
+ * @a: pointr
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		putchar('\n');
+	}
+}
