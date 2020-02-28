79d4a19f14ea17e5cf45fbf0ed8aedbde10d7283
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-28 21:01:09 +0000

    tasks 0 and 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - prints character
+* @argc: print
+* @argv: positions
+*
+* Return: 0
+*/
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
