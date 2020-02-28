e95b468171b74af6c6011c6f01016678c3d8176c
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-02-28 21:40:22 +0000

    trasks 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - prints character
+  * @argc: print
+  * @argv: positions
+  *
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+	return (0);
+}
