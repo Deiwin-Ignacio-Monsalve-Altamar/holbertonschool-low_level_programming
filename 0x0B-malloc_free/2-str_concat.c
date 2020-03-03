dbe6236e814d4b1472524d3eddcf7b9162b8b200
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-03 19:36:57 +0000

    tasks 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 
 	if (s1 == NULL || s2 == NULL)
-		return (NULL)
+		return (NULL);
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
