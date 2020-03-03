c52bda6dffdd81d1046fe4f8adfcc8d6c042d63c
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-03 18:15:05 +0000

    tasks 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* *_strdup - print character
+* @str: pointr character
+* Return: *char
+*/
+char *_strdup(char *str)
+{
+	int i, x;
+	char *dst;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	dst = malloc(sizeof(char) * (i + 1));
+	if (dst == NULL)
+		return (0);
+	for (x = 0; str[x] != '\0'; x++)
+		dst[x] = str[x];
+	return (dst);
+}
