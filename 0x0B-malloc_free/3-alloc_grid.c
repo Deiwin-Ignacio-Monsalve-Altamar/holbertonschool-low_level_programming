e5f73d395ad9d214758671fb9fedbbb26e5b65c0
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-03 22:12:31 -0500

    tasks testing 5

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,32 +1,46 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-  * **alloc_grid - print character
-  * @width: pointr character
-  * @height: poinr character
-  * Return:int array
-  **/
+ * alloc_grid - eturns a ptr to a 2 dimen array of int
+ * @width: input value int
+ * @height: input value int
+ * Return: int
+ */
+
 int **alloc_grid(int width, int height)
 {
-	int i;
-	int **array;
 
-	if (width < 1 || height < 1)
+	int **w;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	w = malloc(height * sizeof(int *));
+
+	if (!w)
 		return (NULL);
 
-	array = malloc(width * sizeof(int *));
-	if (array == NULL)
-		return (array);
-	else
+	for (i = 0; i < height; i++)
 	{
-	for (i = 0; i < width; i++)
-	{
-		array[i] = malloc(height * sizeof(int));
-		if (array[i] == NULL)
+		w[i] = malloc(width * sizeof(int));
+		if (w[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(w[j]);
+			}
+			free(w);
+
 			return (NULL);
+		}
+
+		for (j = 0; j < height; j++)
+		{
+			w[i][j]	= 0;
+		}
 	}
-	free(array[i]);
-	}
-	return (array);
-	free(array);
+	return (w);
 }
