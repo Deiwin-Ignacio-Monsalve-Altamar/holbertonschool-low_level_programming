5872ad53cd2be8671b1249977835766776751882
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-05 19:16:17 +0000

    tasks 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,12 +8,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *s;
+	unsigned int i;
+	char *s;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		s[i] = 0;
 	return (s);
 }
