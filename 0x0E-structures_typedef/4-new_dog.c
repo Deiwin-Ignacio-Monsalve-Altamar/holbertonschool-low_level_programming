faf1ffc447838552b59e5b0d21d2c4ab1bc6e4d2
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-09 22:47:58 -0500

    tasks testing

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,21 +16,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (dog);
 	for (i = 0; name[i]; i++)
-	(*dog).name = malloc(sizeof(char) * i);
+	(*dog).name = malloc(sizeof(char) * i + 1);
 		if ((*dog).name == NULL)
 			free((*dog).name);
 	for (i = 0; owner[i]; i++)
-	(*dog).owner = malloc(sizeof(char) * i);
+	(*dog).owner = malloc(sizeof(char) * i + 1);
 		if ((*dog).owner == NULL)
 		{
 			free((*dog).owner);
 			free(dog);
+			return (NULL);
 		}
 	for (j = 0; (*dog).name[j]; j++)
 		(*dog).name[j] = name[j];
 	(*dog).age = age;
 	for (j = 0; (*dog).owner[j]; j++)
 		(*dog).owner[j] = owner[j];
+
 	return (dog);
 
 }
