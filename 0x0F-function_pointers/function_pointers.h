cf98a8a34805adf4027d004a35f86525da68c21b
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-11 17:36:00 +0000

    tasks first

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUCTION_POINTERS
 #define FUCTION_POINTERS
 void print_name(char *name, void (*f)(char *));
+typedef __SIZE_TYPE__ size_t;
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int op_add(int a, int b);
