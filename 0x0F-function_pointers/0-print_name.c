a8691150994419df8509aa721c254eb971436c10
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-11 16:04:37 +0000

    tasks first

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+* print_name - print a name in uppercase
+* @name: name of the person
+* @(*f)(char *) :  fuctions pointers
+* Return: Nothing.
+*@f : pointers
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name)
+		(*f)(name);
+}
