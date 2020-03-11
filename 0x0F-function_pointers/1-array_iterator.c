e78cb8273ab54eb72e3b5dc0e116fc9f12fef247
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-11 17:39:38 +0000

    tasks first

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+  * array_iterator - print a name in uppercase
+  * @array: int array
+  * @size : type size_t
+  * Return: Nothing.
+  *@action : pointers
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL || size != NULL || action != NULL)
+	{
+		size_t p;
+		for (p = 0; p < size; p++)
+			action(array[p]);
+	}
+}
