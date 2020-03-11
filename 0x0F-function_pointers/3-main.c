289c5f593c02ed62e4220c14f8f3c2502346829f
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-11 22:16:52 +0000

    tasks commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+   * main - check the code for Holberton School students.
+    *
+     * Return: Always 0.
+      */
+int main(argc , argv[])
+{
+	if (argc > 1)
+	{
+		:
+	}
+
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,4 +9,43 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
-
+/**
+ * op_sub - resta
+ * @b: integer
+ * @a: integer
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multi
+ * @b: integer
+ * @a: integer
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @b: integer
+ * @a: integer
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - modulo
+  * @b: integer
+  * @a: integer
+  * Return: int
+  */
+int op_mod(int a, int b)
+{
+		return (a % b);
+}
