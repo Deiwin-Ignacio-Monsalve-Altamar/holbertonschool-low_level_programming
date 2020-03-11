649e4059f8d591c7d6428ce553553ea3b0e0ec9d
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-11 17:45:19 +0000

    tasks testing 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,9 +8,10 @@
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL && size != NULL && action != NULL)
+	if (array != 0 && size != 0 && action != 0)
 	{
 		size_t p;
+
 		for (p = 0; p < size; p++)
 			action(array[p]);
 	}
