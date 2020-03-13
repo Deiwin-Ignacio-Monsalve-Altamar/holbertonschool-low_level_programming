39f3fcc01b35fd077562031dff894de4a60ad47d
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-13 16:10:58 +0000

    tasks 1

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+  * sum_them_all - print sum
+  * @n: const unsigned integer
+  * Return: integer
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	 int suma = 0;
+	 unsigned int i;
+	  va_list a_ap;
+	if (n == 0)
+	{
+
+		return (0);
+	}
+
+	va_start(a_ap, n);
+
+	for (i = 0; i < n; i++)
+		suma += va_arg(a_ap, int);
+
+	va_end(a_ap);
+	return (suma);
+}
