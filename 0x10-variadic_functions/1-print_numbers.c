bbaf3eaac93e32ff5683ebb336183f3db75b7728
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-14 17:37:37 +0000

    tasks 3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,19 +10,21 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i = 1;
 
-	if (separator != NULL)
+	va_start(ap, n);
+	while (i <= n)
 	{
-		va_start(ap, n);
-		while (i <= n)
+		if (separator != NULL)
 		{
 			printf("%d", va_arg(ap, const unsigned int));
 			if (i < n)
 				printf("%s", separator);
-			i++;
 		}
-		putchar('\n');
+		else
+			 printf("%d", va_arg(ap, const unsigned int));
+
+		i++;
 	}
-	else
-		return;
+	putchar('\n');
+
 	va_end(ap);
 }
