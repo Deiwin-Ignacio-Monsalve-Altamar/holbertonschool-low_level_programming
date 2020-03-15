792f72fcead84e651cddb6738fd27779776a114b
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-14 22:01:18 -0500

    Real programmers can write assembly code in any language

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,17 +1,26 @@
-section .text
+    section .data
+fmt     db "%u  %s",10,0
+msg1    db "Hello,",0
+msg2    db "Holberton",0
+
+    section .text
+    extern printf
     global _start
 
 _start:
+    mov  edx, msg1
+    mov  esi, 1
+    mov  edi, fmt
+    mov  eax, 0
+    call printf
 
-        mov     edx,txtlong
-        mov     ecx,texto
-	mov     ebx,1
-        mov     eax,4
-        int     0x80
+    mov  edx, msg2
+    mov  esi, 2
+    mov  edi, fmt
+    mov  eax, 0
+    call printf
 
-        mov     eax,1
-        int     0x80
+    mov  ebx, 0
+    mov  eax, 1
+    int  0x80
 
-section .data
-texto   db      'Hola, Holberton', 0xa
-txtlong equ     $ - texto
