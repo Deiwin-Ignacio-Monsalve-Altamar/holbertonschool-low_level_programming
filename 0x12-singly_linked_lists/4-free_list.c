3a2828a3955f240b524a3a0f7cd84bbfffb6562e
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-19 18:05:08 -0500

    tasks free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_list -  free print cotaing in list
+ * @head: pointrs list_t
+ * Return: list
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	while (head->next != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+	free(head->str);
+	free(head);
+}
