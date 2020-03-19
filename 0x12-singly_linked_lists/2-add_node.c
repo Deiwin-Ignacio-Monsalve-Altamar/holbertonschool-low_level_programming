e7a38d343552a393ee8faa4d7dbeb7c1b9605e88
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-19 15:16:33 -0500

    tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <string.h>
 /**
  * add_node - print cotaing in list
  * @head: pointrs list_t
@@ -28,8 +27,24 @@ list_t *add_node(list_t **head, const char *str)
 		(*new).next = *head;
 	}
 	(*new).str = strdup(str);
-	(*new).len = strlen(str);
+	(*new).len = _strlen(str);
 	*head = new;
 
 	return (*head);
+}
+/**
+ * _strlen - logitud strings
+ * @str: const char
+ * Return: int
+ */
+int _strlen(const char *str)
+{
+	int i;
+
+	while (*str)
+	{
+		i++;
+		str++;
+	}
+	return (i);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -24,6 +24,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+int _strlen(const char *str);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
