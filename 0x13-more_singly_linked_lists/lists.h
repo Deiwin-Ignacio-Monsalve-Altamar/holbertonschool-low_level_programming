d6bcfbd24603bdd56f4489060dc87e6f1618a4cf
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-23 08:46:33 -0500

    Fix struct

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -7,17 +7,17 @@
 #include <string.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+* for Holberton project
+*/
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 /* function that prints all the elements of a listint_t list.*/
@@ -38,13 +38,13 @@ void free_listint(listint_t *head);
 /* function that frees a listint_t list.*/
 void free_listint2(listint_t **head);
 
-/* function that deletes the head node of a listint_t linked list, and returns the head node’s data (n).*/
+/* function that deletes the head node of a list, returns */
 int pop_listint(listint_t **head);
 
 /* function that returns the nth node of a listint_t linked list.*/
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
-/* function that returns the sum of all the data (n) of a listint_t linked list.*/
+/* function that returns the sum of all the data (n) of a list.*/
 int sum_listint(listint_t *head);
 
 /* function that inserts a new node at a given position.*/
