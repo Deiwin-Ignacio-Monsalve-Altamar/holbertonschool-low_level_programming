fcecfa9b4c9a625dbc1437f02e28a2f57635d628
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-23 08:29:53 -0500

    Update of file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -54,3 +54,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 
 /* function that deletes the node at index index of a listint_t linked list.*/
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
+#endif
