563cd5e41c307d80775251d6880aba984fabb9ae
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-25 15:17:13 -0500

    tasks advanced free safe

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+*free_listint_safe - adding data a list
+*@head: pointer
+*Return: size_t
+*/
+size_t free_listint_safe(listint_t **h)
+{
+    size_t len = 0;
+
+	if (*h == NULL)
+	{
+		return (0);
+	}
+	while (*h)
+	{
+		len++;
+		if (*h - (*h)->next > 0)
+		{
+			free(*h);
+		}
+		else
+		{
+            free(*h);
+			break;
+		}
+		*h = (*h)->next;
+	}
+    *h = NULL;
+	return (len);
+}
