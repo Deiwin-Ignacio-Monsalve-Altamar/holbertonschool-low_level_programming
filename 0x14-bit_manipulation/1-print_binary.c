fc2cc4e3f2796ff5308745a528979c270aa51c90
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-26 13:47:53 -0500

    function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+*print_binary - print numbers binarys
+*@n: unsigned long int
+*Return: 0
+*/
+void print_binary(unsigned long int n)
+{
+	int c;
+	unsigned int result;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	for (c = 32; c >= 0; c--)
+	{
+		result = n >> c;
+
+		if (result & 1)
+			_putchar('1');
+		else if (result | 0)
+			_putchar('0');
+	}
+}
