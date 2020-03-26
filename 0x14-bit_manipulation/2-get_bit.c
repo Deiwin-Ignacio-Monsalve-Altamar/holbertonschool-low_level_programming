346ac494d213fce4f7336d7ad3b5e04552d9bbca
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-26 16:29:25 -0500

    función que establezca el valor de un bit 1en un índice dado

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,14 +7,14 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int check = 1;
+	unsigned long int tmp = 1;
 
 	if (index > sizeof(n) * 8)
 	{
 		return (-1);
 	}
-	check <<= index;
-	if (check & n)
+	tmp <<= index;
+	if (tmp & n)
 	{
 		return (1);
 	}
