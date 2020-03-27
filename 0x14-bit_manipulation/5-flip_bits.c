68af0a3bd74d97b04843fc50a497debdbbdba227
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-26 21:08:11 -0500

    function that returns the number of bits you would need to flip to get from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+*flip_bits - print numbers binarys
+*@n: unsigned int pointrs
+*@m: unsigned int
+*Return: unsigned long int
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int tmp, cont;
+
+	tmp = n ^ m;
+
+	for (cont = 0; aux > 0)
+	{
+		if ((aux & 1) == 1)
+		{
+			cont++;
+		}
+		aux = aux >> 1;
+	}
+	return (cont);
+
+}
