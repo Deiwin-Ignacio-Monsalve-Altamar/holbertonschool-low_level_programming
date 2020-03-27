bb518bfd83916615aaa9afc7a7ffa7c1734becac
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-26 22:58:35 -0500

    tasks advanced

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+*get_endianness - print numbers binarys
+*@void: unsigned int pointrs
+*
+*Return: int
+*/
+int get_endianness(void)
+{
+	int x, litle_or_big;
+
+	x = 0;
+
+	litle_or_big = (int)(((char *)&x)[0]);
+	return (litle_or_big);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -19,4 +19,5 @@ int clear_bit(unsigned long int *n, unsigned int index);
 /*  function that returns the number of bits  get from one number to another.*/
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
+int get_endianness(void);
 #endif
