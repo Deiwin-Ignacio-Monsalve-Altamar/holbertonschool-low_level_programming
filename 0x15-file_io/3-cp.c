761023496ed7178e002e7a3faf5d771d209e52c9
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-03-31 20:26:33 -0500

    tasks 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,83 +1,66 @@
 #include "holberton.h"
-void _close(int file1, int file2, char *s, char *p);
-void copy(int file1, int file2, char *s, char *p);
+void _close(int file);
 /**
- *main - adding or coping file a to file
- *@argc: const char
- *@argv: files
- * Return: 0
+ * main- copy file
+ * @argc: size av
+ * @av: string
+ * Return: int
  */
-int main(int argc, char *argv[])
+int main(int argc, char *av[])
 {
-	int file1, file2;
-	char *s = argv[1], *p = argv[2];
+	int filef, filet, filer, filew;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	file1 = open(argv[1], O_RDONLY);
-	if (file1 == -1)
+	filef = open(av[1], O_RDONLY);
+	if (filef == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(97);
-	}
-	file2 = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	if (file2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	copy(file1, file2, s, p);
-	return (0);
-}
-/**
- *copy - adding or coping file1 a to file2
- *@file1: const char
- *@file2: files
- *@s: pointer
- *@p: pointer
- * Return: 0
- */
-void copy(int file1, int file2, char *s, char *p)
-{
-	int rd, wr;
-	char buffer[1024];
-
+	filet = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (filet == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 	do {
-		rd = read(file1, buffer, 1024);
-		if (rd == -1)
+		filer = read(filef, buffer, 1024);
+		if (filer == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", s);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
-		wr = write(file2, buffer, rd);
-		if (wr == -1)
+		filew = write(filet, buffer, filer);
+		if (filew == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", p);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-	} while (file1 == 1024);
-	_close(file1, file2, s, p);
+	} while (filer == 1024);
+	_close(filef);
+	_close(filet);
+	return (0);
 }
+
+
 /**
- *close - adding or coping file1 a to file2
- *@file1: const char
- *@file2: files
- * Return: 0
+ * _close - close file
+ * @file: fd of file tu close
+ * Return: void
  */
-void _close(int file1, int file2, char *s, char *p)
+void _close(int file)
 {
-	if (close(file1) == -1)
+	int fail;
+
+	fail = close(file);
+	if (fail == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", s);
-		exit(100);
-	}
-	if (close(file2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", p);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
 		exit(100);
 	}
 }
