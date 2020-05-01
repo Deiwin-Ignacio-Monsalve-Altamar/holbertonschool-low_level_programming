35a76896e8d547d9afcaa4b8777b23cea6d03273
Deiwin-Ignacio-Monsalve-Altamar <1651@hobertonschool.com>
2020-05-01 18:01:55 +0000

    adds a new node at the end of a dlistint_t list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,11 +1,34 @@
 #include "lists.h"
-
 /**
- * add_dnodeint_end -
- *
- * Return: 
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: d_listint_t
+ * @n: integer
+ * Return: d_listint_t
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
+	dlistint_t *new = NULL, *tmp = NULL;
 
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	tmp = *head;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->prev = NULL;
+		return (new);
+	}
+
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = new;
+	new->prev = tmp;
+	return (new);
 }
