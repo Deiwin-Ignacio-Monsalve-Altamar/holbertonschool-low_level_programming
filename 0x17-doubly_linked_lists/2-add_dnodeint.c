f5b710deda9e11b2bb7aad0241b99207b76395e4
Deiwin-Ignacio-Monsalve-Altamar <1651@hobertonschool.com>
2020-05-01 17:37:35 +0000

    node at the beginning of a dlistint_t list.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,29 @@
 #include "lists.h"
-
 /**
- * add_dnodeint -
- *
- * Return: 
+ * add_dnodeint - node at the beginning of a dlistint_t list.
+ * @head: dlistint_t
+ * @n: int const
+ * Return: dlistint_t
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new = NULL, *tmp = NULL;
 
+	if (head != NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+
+	tmp = *head;
+	*head = new;
+	new->next = tmp;
+
+	if (tmp != NULL)
+	{
+		tmp->prev = *head;
+	}
+	return (new);
 }
