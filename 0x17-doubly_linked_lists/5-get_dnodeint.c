65e95ea947f2b0cb8c8f08af524a6a0285ef33b3
Deiwin-Ignacio-Monsalve-Altamar <1651@hobertonschool.com>
2020-05-01 19:59:03 +0000

    function that returns the nth node of

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - function that returns the nth node of
+ * @head: dlisint_t
+ * @index: unsigned int
+ * Return: dlistint
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int aux = 0;
 
+	if (head == NULL)
+		return (NULL);
+	while (head->next != NULL)
+	{
+		if (aux == index)
+		{
+			break;
+		}
+		head = head->next;
+		aux++;
+	}
+	return (head);
 }
