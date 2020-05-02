b9b4d746113a91f627c7477510dd8abd68c386ac
Deiwin-Ignacio-Monsalve-Altamar <1651@hobertonschool.com>
2020-05-02 23:30:41 +0000

    function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,42 +9,43 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int auxposition = 0;
-	dlistint_t *add_nodo = NULL, *temp = NULL, *temp_header = NULL;
+	dlistint_t *new, *tmp, *con;
+	unsigned int i = 0, j = 0;
 
-	if (h == NULL)
-		return (NULL);
+	con = *h;
 
-	temp_header = *h;
-	while (temp_header != NULL)
+	while (con)
 	{
-		auxposition++;
-		temp_header = temp_header->next;
+		con = con->next;
+		j++;
 	}
-	if (idx > auxposition)
+	if (idx > j)
 		return (NULL);
-
-	add_nodo = malloc(sizeof(dlistint_t));
-	if (add_nodo == NULL)
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
 		return (NULL);
-
-	add_nodo->n = n;
+	}
+	new->n = n;
 	if (idx == 0)
 	{
-		add_nodo->next = *h;
-		add_nodo->prev = NULL;
-		*h = add_nodo;
-		return (add_nodo);
+		new->next = *h;
+		new->prev = NULL;
+		*h = new;
+		return (new);
 	}
-	temp = *h;
-	auxposition = 0;
-	while (auxposition < idx && temp->next)
+	if (h == NULL)
 	{
-		temp  = temp->next;
-		auxposition++;
+		return (NULL);
 	}
-	add_nodo->next = temp->next;
-	temp->next = add_nodo;
-	add_nodo->prev = temp;
-	return (add_nodo);
+	tmp = *h;
+	while (i < idx - 1 && tmp->next)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	new->next = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+	return (new);
 }
