6a3a24179bd147cb2aed84e1a027d1f250fa86e2
Deiwin-Ignacio-Monsalve-Altamar <1651@hobertonschool.com>
2020-05-03 16:00:10 +0000

    function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,8 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 	add_nodo = malloc(sizeof(dlistint_t));
-	if (add_nodo == NULL)
-		return (NULL);
 	add_nodo->n = n;
 	if (!idx && *h == NULL)
 	{
@@ -44,8 +42,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			add_nodo->next = temp;
 			return (add_nodo);
 		}
+		if (temp->next == NULL && ++auxposition == idx)
+			return (add_nodo_insert(temp, add_nodo));
 		auxposition++;
 		temp  = temp->next;
 	}
 	return (NULL);
+}
+/**
+ * add_nodo_insert - inserts a new node at a given position.
+ * @temp: dlistint
+ * @add_nodo: dlistint
+ * Return: dlistint_t
+ */
+dlistint_t *add_nodo_insert(dlistint_t *temp, dlistint_t *add_nodo)
+{
+	temp->next = add_nodo;
+	add_nodo->next = NULL;
+	add_nodo->prev = temp;
+	return (add_nodo);
 }
