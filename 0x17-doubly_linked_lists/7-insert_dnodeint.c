67a0a8afcebe3f15c2aeca6923fc339076574d7e
Deiwin-Ignacio-Monsalve-Altamar <1651@hobertonschool.com>
2020-05-03 15:46:43 +0000

    function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: dlistint variables struct
@@ -14,25 +13,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
-	add_nodo = malloc(sizeof(dlistint_t));
+	add_nodo = add_nodo_insert(h, idx, n);
 	if (add_nodo == NULL)
 		return (NULL);
-	add_nodo->n = n;
-	if (!idx && *h == NULL)
-	{
-		add_nodo->prev = NULL;
-		add_nodo->next = NULL;
-		*h = add_nodo;
-		return (add_nodo);
-	}
-	if (idx == 0)
-	{
-		add_nodo->next = *h;
-		add_nodo->prev = NULL;
-		(*h)->prev = add_nodo;
-		*h = add_nodo;
-		return (add_nodo);
-	}
 	temp = *h;
 	auxposition = 0;
 	while (temp)
@@ -56,4 +39,36 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp  = temp->next;
 	}
 	return (NULL);
+}
+/**
+ * add_nodo_insert - inserts a new node at a given position.
+ * @h: dlistint variables struct
+ * @idx: unsigned int
+ * @n: int
+ * Return: dlistint_t
+ */
+dlistint_t *add_nodo_insert(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *add_nodo = NULL;
+
+	add_nodo = malloc(sizeof(dlistint_t));
+	if (add_nodo == NULL)
+		return (NULL);
+	add_nodo->n = n;
+	if (!idx && *h == NULL)
+	{
+		add_nodo->prev = NULL;
+		add_nodo->next = NULL;
+		*h = add_nodo;
+		return (add_nodo);
+	}
+	if (idx == 0)
+	{
+		add_nodo->next = *h;
+		add_nodo->prev = NULL;
+		(*h)->prev = add_nodo;
+		*h = add_nodo;
+		return (add_nodo);
+	}
+	return (add_nodo);
 }
