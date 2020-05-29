71be96b3c7136b57fb44f8f0956dae828ad7b37b
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-29 15:41:39 +0000

    function that gives you the index of a key.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,25 +10,23 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx = 0;
-	hash_node_t *new_node, *tmp;
+	hash_node_t *new_node;
+	hash_node_t *tmp;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 	    key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 	idx = key_index((const unsigned char *) key, ht->size);
 	tmp = ht->array[idx];
-	if (tmp != NULL)
+	while (tmp != NULL)
 	{
-		while (tmp != NULL)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			if (strcmp(tmp->key, key) == 0)
-			{
-				free(tmp->value);
-				tmp->value = strdup(value);
-				return (1);
-			}
-			tmp = tmp->next;
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
 		}
+		tmp = tmp->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
