ddb80605c271959d8e9bf238a88a3896404fb6fe
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-29 16:34:24 +0000

    free list int

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,5 +7,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
+    unsigned int index;
+    hash_node_t *runner, *tmp;
 
+    if (ht == NULL)
+        return;
+    index = 0;
+    while (index < ht->size)
+    {
+        runner = ht->array[index];
+        while (runner)
+        {
+            free(runner->key);
+            free(runner->value);
+            tmp = runner->next;
+            free(runner);
+            runner = tmp;
+        }
+        ++index;
+    }
+    free(ht->array);
+    free(ht);
 }
