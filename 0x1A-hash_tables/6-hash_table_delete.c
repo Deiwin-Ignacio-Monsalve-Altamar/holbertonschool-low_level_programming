367a827eb3754c4d2a44e5b34657ad4ed0dabdca
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-29 16:35:48 +0000

    free list int

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,31 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete -
- *
+ * hash_table_delete - free tables
+ * @ht: pointr character
  * Return: 
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned int index;
-    hash_node_t *runner, *tmp;
+	unsigned int index;
+	hash_node_t *runner, *tmp;
 
-    if (ht == NULL)
-        return;
-    index = 0;
-    while (index < ht->size)
-    {
-        runner = ht->array[index];
-        while (runner)
-        {
-            free(runner->key);
-            free(runner->value);
-            tmp = runner->next;
-            free(runner);
-            runner = tmp;
-        }
-        ++index;
-    }
-    free(ht->array);
-    free(ht);
+	if (ht == NULL)
+		return;
+	index = 0;
+	while (index < ht->size)
+	{
+		runner = ht->array[index];
+		while (runner)
+		{
+			free(runner->key);
+			free(runner->value);
+			tmp = runner->next;
+			free(runner);
+			runner = tmp;
+		}
+		++index;
+	}
+	free(ht->array);
+	free(ht);
 }
