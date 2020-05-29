9968426f208ced0d126dc3958a87bc2220c1f208
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-05-29 16:04:48 +0000

    function that gives you the index of a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,23 +4,23 @@
  * hash_table_get - get data
  * @ht: header list
  * @key: const char
- * Return: 
+ * Return: pointe char
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *tmp;
+	unsigned long int index;
+	hash_node_t *tmp;
 
-    if (ht == NULL || ht->size == 0 || !strlen(key))
-        return (NULL);
-    
-    index = key_index((const unsigned char *)key, ht->size);
-    tmp = ht->array[index];
-    while (tmp)
-    {
-        if (strcmp(tmp->key, key) == 0)
-            return tmp->value;
-        tmp = tmp->next;
-    }
-    return (NULL);
+	if (ht == NULL || ht->size == 0 || !strlen(key))
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
