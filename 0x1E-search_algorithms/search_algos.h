68847c0433cf50542ef29435dfd4091a6b453347
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-26 12:20:44 -0500

    fix first commit

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,15 +1,8 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
-/*Libraries standars*/
-#include <stdio.h>
-#include <stdlib.h>
-
-/*Function algorithms search*/
+#ifndef SEARCH_ALGORITHMS_H
+#define SEARCH_ALGORITHMS_H
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int left, int right, int *array);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-
 #endif
